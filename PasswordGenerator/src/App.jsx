7a7368e25ae3b907354b4ed045cc7cb7dd6b2377
coder-shahjalal <shahjalal.freelancer@gmail.com>
2024-01-04 22:15:53 +0600

    passwordGenerator

---
+++ PasswordGenerator/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setnumberAllowed] = useState(false);
+  const [caratorAllowed, setcaratorAllowed] = useState(false);
+  const [password, setpassword] = useState(false);
+  con
+  return (
+    <>
+    <h1 className='text-4xl text-center text-white'>
+     Password Generator
+    </h1>
+    </>
+  )
+}
+
+export default App
---
+++ bgChanger2/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [color,setColor] =useState("black")
+  return (
+    <div className='w-full h-screen' style={{backgroundColor:color}}>
+      <div className=' bg-zinc-300 rounded-full justify-center fixed mx-[50rem] my-[50rem] px-3 py-2 flex text-white font-semibold'>
+          <button onClick={()=>setColor("red")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg'style={{backgroundColor: "red"}} >Red</button>
+          <button onClick={()=>setColor("green")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg bg-red' style={{backgroundColor: "green"}}>Green</button>
+          <button onClick={()=>setColor("blue")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg' style={{backgroundColor: "blue"}}>Blue</button>
+          <button onClick={()=>setColor("gray")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg' style={{backgroundColor: "gray"}}>Gray</button>
+          <button onClick={()=>setColor("orange")} className='px-5 py-2 rounded-3xl drop-shadow-lg' style={{backgroundColor: "orange"}}>Orange</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
