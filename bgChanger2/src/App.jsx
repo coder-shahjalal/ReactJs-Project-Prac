bebf058f3165c1b0ed6fd3d60a42ae6a0203d77f
coder-shahjalal <shahjalal.freelancer@gmail.com>
2024-01-05 18:58:31 +0600

    bgchenger2 button color tailwind

--- bgChanger2/src/App.jsx
+++ bgChanger2/src/App.jsx
@@ -5,11 +5,11 @@ const App = () => {
   return (
     <div className='w-full h-screen' style={{backgroundColor:color}}>
       <div className=' bg-zinc-300 rounded-full justify-center fixed mx-[50rem] my-[50rem] px-3 py-2 flex text-white font-semibold'>
-          <button onClick={()=>setColor("red")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg'style={{backgroundColor: "red"}} >Red</button>
-          <button onClick={()=>setColor("green")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg bg-red' style={{backgroundColor: "green"}}>Green</button>
-          <button onClick={()=>setColor("blue")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg' style={{backgroundColor: "blue"}}>Blue</button>
-          <button onClick={()=>setColor("gray")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg' style={{backgroundColor: "gray"}}>Gray</button>
-          <button onClick={()=>setColor("orange")} className='px-5 py-2 rounded-3xl drop-shadow-lg' style={{backgroundColor: "orange"}}>Orange</button>
+          <button onClick={()=>setColor("red")} className='px-5 py-2 rounded-3xl mr-2 drop-shadow-lg bg-[red]' >Red</button>
+          <button onClick={()=>setColor("green")} className='px-5 py-2 mr-2 rounded-3xl drop-shadow-lg bg-[green]' >Green</button>
+          <button onClick={()=>setColor("blue")} className='px-5 py-2 mr-2 rounded-3xl drop-shadow-lg bg-[blue]'>Blue</button>
+          <button onClick={()=>setColor("gray")} className='px-5 py-2 mr-2 rounded-3xl drop-shadow-lg bg-[gray]'>Gray</button>
+          <button onClick={()=>setColor("orange")} className='px-5 py-2 rounded-3xl drop-shadow-lg bg-[orange]' >Orange</button>
       </div>
     </div>
   )
