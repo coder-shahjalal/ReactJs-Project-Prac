7b24d36844e92b1d48acf62cff0ab27cd0013281
coder-shahjalal <shahjalal.freelancer@gmail.com>
2024-01-07 18:02:27 +0600

    random pass ready and num char

--- PasswordGenerator/src/App.jsx
+++ PasswordGenerator/src/App.jsx
@@ -1,32 +1,49 @@
 
 
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 
 const App = () => {
   const [length, setLength] = useState(8);
   const [numberAllowed, setnumberAllowed] = useState(false);
   const [caratorAllowed, setcaratorAllowed] = useState(false);
-  const [password, setpassword] = useState();
+  const [password, setPassword] = useState("");
+
+
   const passwordGenerator = useCallback(() => {
     let pass = "";
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     if (numberAllowed) str += "0123456789";
     if (caratorAllowed) str += "!@#$%^&*()_+-=[]{}|;:,.<>?";
-    for (let i = 1; i <= Array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
-    setpassword(pass);
-  }, [length, numberAllowed, caratorAllowed, setpassword]);
+    
+    setPassword(pass)
+  }, [length, numberAllowed, caratorAllowed, setPassword]);
+  
+// Use Raf
+  const passwordRef = useRef()
+    
+    useEffect(()=>{
+      passwordGenerator()
+
+      
+    },[length,numberAllowed,caratorAllowed,passwordGenerator])
+
+    const copyPasswordToClipboard =useCallback(()=>{
+      window.navigator.clipboard.writeText(password)
+    },[password])
+  
   return (
     <>
 
     <h1 className='text-4xl text-center text-white bg-zinc-800'>
     Password Generator
     </h1>
-    =======
+
       <div className="w-full max-w-md px-4 py-3 mx-auto my-8 text-orange-500 bg-gray-700 rounded-lg shadow-md">
         <h1 className="my-3 text-center text-white">Password Generator</h1>
         <div className="flex mb-4 overflow-hidden rounded-lg shadow">
@@ -36,8 +53,9 @@ const App = () => {
             className="w-full px-3 py-1 outline-none"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
           />
-          <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          <button onClick={copyPasswordToClipboard} className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
             Copy
           </button>
         </div>
@@ -51,6 +69,7 @@ const App = () => {
               className="cursor-pointer"
               onChange={(e) => {
                 setLength(e.target.value);
+                
               }}
             />
             <label>Length:{length}</label>
@@ -72,7 +91,7 @@ const App = () => {
               defaultChecked={caratorAllowed}
               id="caraInput"
               onChange={() => {
-                caratorAllowed((prev) => !prev);
+                setcaratorAllowed((prev) => !prev);
               }}
             />
             <label htmlFor="caraInput">Characters</label>
