d9bfb79eaa2811c6b1319adfd4bf54c8ddf7dc6b
coder-shahjalal <shahjalal.freelancer@gmail.com>
2024-01-08 22:47:56 +0600

    min added

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className='text-center text-white bg-sky-600 ' >
-      reactjs test tailwind
+      reactjs test tailwindd
     </div>
   )
 }
