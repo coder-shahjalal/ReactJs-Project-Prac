ea1dcaf808e4c9e5526feaca34fe47d5e6bfd1b1
md shahjalal <shahjalal.freelancer@gmail.com>
2024-01-15 17:33:00 +0600

    currency converter

--- currencyConverter/src/hooks/useCurrencyInfo.js
+++ currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+
 
 function useCurrencyInfo(currency){
-    
-    const [data,setData]=useState({})
-    useEffect(()=>{
+    const [data, setData] = useState({})
+    useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res)=>res.json())
-        .catch((res)=>setData(res[currency]))
+        .then((res) => res.json())
+        .then((res) => setData(res[currency]))
         console.log(data);
-    },[currency])
+    }, [currency])
+    console.log(data);
     return data
 }
 
