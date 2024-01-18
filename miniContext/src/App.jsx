3b505a2f32ac9c42a836490da035f03c95e8c96e
md shahjalal <shahjalal.freelancer@gmail.com>
2024-01-18 19:10:24 +0600

    theme switcher

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+// import { useState } from "react";
 import "./App.css";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import UserContextProvider from "./context/UserContextProvied";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <UserContextProvider>
