d15ec6ba95a53d2a7b769f0dfaf8c4e28cf811ea
md shahjalal <shahjalal.freelancer@gmail.com>
2024-01-23 13:17:31 +0600

    home ready

--- pracContextRouter/src/components/Home.jsx
+++ pracContextRouter/src/components/Home.jsx
@@ -1,10 +1,9 @@
-import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 
 const Home = () => {
   return (
-    <div className=" mt-6 border-y-4 dark:rounded-lg dark:border-slate-500 dark:border-y-6 border-slate-200 dark:bg-gray-800 dark:border-gray-700 py-6 text-center  dark:bg-gray-800 dark:border-gray-700">
+    <div className=" mt-6 border-y-4 dark:rounded-lg dark:border-slate-500 dark:border-y-6  border-slate-200 dark:bg-gray-800 dark:border-gray-700 py-6 text-center  dark:bg-gray-800 dark:border-gray-700">
       <Header />
       <Footer />
     </div>
