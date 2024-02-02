3fbc47c0eea350c7e9d40210d4942ecf75ea02c0
coder-shahjalal <shahjalal.freelancer@gmail.com>
2024-02-02 22:54:49 +0400

    mega blog

---
+++ MegaBolgAppwrite/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+     <div>A blog app with appwrite</div>
+    </>
+  )
+}
+
+export default App
---
+++ MegaBolgAppwrite/src/appwrite/auth.js
@@ -0,0 +1,9 @@
+//import conf from "../conf/conf";
+
+//import {Client,Account,ID} from "appwrite"
+
+export class AuthService {}
+
+const authService = new AuthService()
+
+export default AuthService
