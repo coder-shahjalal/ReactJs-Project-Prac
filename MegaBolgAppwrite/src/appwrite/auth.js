3efee658b3c6fafa405a386da2591c6028358485
md shahjalal <shahjalal.freelancer@gmail.com>
2024-02-03 17:18:10 +0600

    auth file ready

--- MegaBolgAppwrite/src/appwrite/auth.js
+++ MegaBolgAppwrite/src/appwrite/auth.js
@@ -1,9 +1,52 @@
-//import conf from "../conf/conf";
+import conf from "../conf/conf";
 
-//import {Client,Account,ID} from "appwrite"
+import { Account, Client, ID } from "appwrite";
 
-export class AuthService {}
+export class AuthService {
+    client = new Client ();
+    account;
 
-const authService = new AuthService()
+    constructor (){
+        this.client
+                .setEndpoint(conf.appwriteUrl)
+                .setProject(conf.appwriteProjectId);
+
+            this.account = new Account (this.client)
+    }
+
+    async createAccount({email,password,name}){
+        try {
+              const userAccount = await this.account.create(ID.unique(),email,name)
+              if(userAccount){
+                return this.login({email,password})
+              }else {
+                return userAccount
+              }
+        }catch (error){
+            throw error
+        }
+    }
+    async login({email,password}){
+        try {
+return await this.account.createEmailSession(email,password);
+        }catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error){
+            throw error
+        }
+
+        return null;
+    }
+}
+
+const authService = new AuthService(
+   
+)
 
 export default AuthService
